2289f3e0254de8625558c176a795be98b832379e
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-03 17:05:15 +0200

    Refactor type fetching and string formatting in field_utils.py
    Implemented a new function to generate artifact type string and made adjustments in get_param_meta_data function to fetch type names accurately. Also, updated the string formatting in get_param_meta_data_str function for clearer code readability.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- pykfp/utils/field_utils.py
+++ pykfp/utils/field_utils.py
@@ -2,24 +2,33 @@ import dataclasses
 from typing import Any, Tuple
 
 
-def get_param_meta_data(field: dataclasses.Field, value: Any) -> Tuple[str, type, Any]:
+def get_artifact_type_string(field_type: type):
+    artifact_type = field_type.__args__[0].__name__
+    side = "Input" if "input" in field_type.__metadata__[0] else "Output"
+
+    return f"{side}[{artifact_type}]"
+
+
+def get_param_meta_data(field: dataclasses.Field, value: Any) -> Tuple[str, str, Any]:
     name = field.name
-    field_type = field.type
+    field_type = field.type.__name__
     try:
         field_value = value if value is not None else field.default_factory()()
     except TypeError:
         field_value = None
+    if field_type == "Annotated":
+        field_type = get_artifact_type_string(field.type)
     return name, field_type, field_value
 
 
 def get_param_meta_data_str(
-    name: str, field_type: type, value: Any, with_typing=True
+    name: str, field_type: str, value: Any, with_typing=True
 ) -> str:
-    if field_type.__name__ == "str":
+    if field_type == "str":
         value = f'"{value}"'
 
     if with_typing:
-        md_str = f"{name}: {field_type.__name__} = {value}"
+        md_str = f"{name}: {field_type} = {value}"
     else:
         md_str = f"{name} = {value}"
     # if field_type.__name__ == 'str':
