9eee794a83290aa29aed6d2877c72fd954f20767
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-03 20:59:27 +0200

    Refactor and enhance 'get_artifact_type_string' method
    The 'get_artifact_type_string' function was refactored to handle more types, including the case when the field type isn't a type but an instance, and vastly improved its type identification logic. This change increases the flexibility of this function and boosts its accuracy in type determination.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- pykfp/utils/field_utils.py
+++ pykfp/utils/field_utils.py
@@ -2,27 +2,36 @@ import dataclasses
 from typing import Any, Tuple
 
 
-def get_artifact_type_string(field_type: type):
-    artifact_type = field_type.__args__[0].__name__
-    side = "Input" if "input" in field_type.__metadata__[0] else "Output"
+def get_artifact_type_string(field_type: Any) -> str:
+    if "Annotated" in str(field_type):
+        artifact_type = (
+            repr(field_type.__args__[0]).split("'")[-2].replace("pykfp.artifacts.", "")
+        )
 
-    return f"{side}[{artifact_type}]"
+        side = "Input" if "input" in field_type.__metadata__[0] else "Output"
+        return f"{side}[{artifact_type}]"
+    if not isinstance(field_type, type):
+        field_type_name = repr(field_type)
+        return field_type_name.replace("typing.", "")
+
+    field_type_name = field_type.__name__
+    return field_type_name
 
 
 def get_param_meta_data(field: dataclasses.Field, value: Any) -> Tuple[str, str, Any]:
     name = field.name
-    field_type = field.type.__name__
     try:
-        field_value = value if value is not None else field.default_factory()()
+        field_value = value
     except TypeError:
         field_value = None
-    if field_type == "Annotated":
-        field_type = get_artifact_type_string(field.type)
+    if callable(field.default_factory) and field_value is None:
+        field_value = field.default_factory()
+    field_type = get_artifact_type_string(field.type)
     return name, field_type, field_value
 
 
 def get_param_meta_data_str(
-    name: str, field_type: str, value: Any, with_typing=True
+    name: str, field_type: str, value: Any, with_typing: bool = True
 ) -> str:
     if field_type == "str":
         value = f'"{value}"'
@@ -31,6 +40,5 @@ def get_param_meta_data_str(
         md_str = f"{name}: {field_type} = {value}"
     else:
         md_str = f"{name} = {value}"
-    # if field_type.__name__ == 'str':
 
     return md_str
