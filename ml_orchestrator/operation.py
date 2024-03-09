913a3eb4a5af7f551a4b31c7f51d5050779e88e3
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-09 21:16:53 +0200

    Refactor operation.py for readability and scalability
    The operation.py script is refactored for better code readability and scalability. Functionality related to the generation of function definitions and import compounds was extracted into helper methods. List comprehensions were also simplified and an import statement was moved to a global constant.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- ml_orchestrator/operation.py
+++ ml_orchestrator/operation.py
@@ -1,3 +1,4 @@
+# Refactored code
 import dataclasses
 from typing import List
 
@@ -9,6 +10,8 @@ from ml_orchestrator.utils.field_utils import (
     get_param_meta_data_str,
 )
 
+IMPORT_COMPOUND = "from kfp.dsl import *\nfrom typing import *"
+
 
 @dataclasses.dataclass
 class Operation:
@@ -18,30 +21,36 @@ class Operation:
     compute_resources: ComputeResources
 
     def create_function(self) -> str:
-        comp_vars = self.component.comp_vars()
+        component_variables = self.component.comp_vars()
         comp_class = self.component.__class__
-        func_scope = "(\n\t" + ",\n\t".join(self.get_func_params(comp_vars)) + "\n)"
-        comp_scope = "(\n\t\t" + ",\n\t\t".join(self.get_comp_params(comp_vars)) + "\n\t)"
-        func_definition = f"def {self.kfp_func_name}{func_scope}:"
-        import_compound = f"from {comp_class.__module__} import {comp_class.__name__}"
+        func_scope = "(\n\t" + ",\n\t".join(self.get_func_params(component_variables)) + "\n)"
+        comp_scope = "(\n\t\t" + ",\n\t\t".join(self.get_comp_params(component_variables)) + "\n\t)"
+        func_definition = self._format_function_definition(func_scope)
+        import_compound = self._format_import_compound(comp_class)
         comp_init = f"comp = {comp_class.__name__}{comp_scope}"
         comp_run = "comp.execute()"
         str_func_body = "\n\t".join([import_compound, comp_init, comp_run])
         str_func = func_definition + "\n\t" + str_func_body
         return str_func
 
+    def _format_function_definition(self, func_scope: str) -> str:
+        return f"def {self.kfp_func_name}{func_scope}:"
+
+    def _format_import_compound(self, comp_class: type) -> str:
+        return f"from {comp_class.__module__} import {comp_class.__name__}"
+
     @staticmethod
     def create_decorator(environment_params: EnvironmentParams) -> str:
         dec_vars = environment_params.comp_vars()
         prams = Operation.get_func_params(dec_vars, with_typing=False)
-        override_params = []
-        for p in prams:
-            if "None" not in p:
-                override_params.append(p)
-
+        override_params = Operation._get_decorator_override_params(prams)
         dec_scope = "(\n\t" + ",\n\t".join(override_params) + "\n)"
         func_definition = f"@component{dec_scope}"
         return func_definition
+
+    @staticmethod
+    def _get_decorator_override_params(prams: List[str]) -> List[str]:
+        return [p for p in prams if "None" not in p]
 
     @staticmethod
     def get_func_params(comp_vars: dict, with_typing: bool = True) -> List[str]:
@@ -59,14 +68,10 @@ class Operation:
             f.write(content)
 
     def create_kfp_str(self) -> str:
-        import_compound = "from kfp.dsl import *\nfrom typing import *"
         environment_params = self.environment_params or self.component.env
         decorator_str = self.create_decorator(environment_params)
         decorator_str = decorator_str.replace(" = ", "=")
-
         function_str = self.create_function()
-
         kfp_component_str = f"{decorator_str}\n{function_str}"
-        kfp_component_str = f"{import_compound}\n{kfp_component_str}"
-
+        kfp_component_str = f"{IMPORT_COMPOUND}\n{kfp_component_str}"
         return kfp_component_str
