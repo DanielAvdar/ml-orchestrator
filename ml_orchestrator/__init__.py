19a01ce08a071b55b3418e94c308c7b8e1209797
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-31 02:24:49 +0300

    Update __all__ variable in ml_orchestrator
    The __all__ variable in the ml_orchestrator __init__.py file has been modified. The "artifacts" element has been removed, leaving only "ComponentParser", "MetaComponent", and "EnvironmentParams".
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- ml_orchestrator/__init__.py
+++ ml_orchestrator/__init__.py
@@ -1,6 +1,9 @@
-from .artifacts import artifacts
 from .comp_parser import ComponentParser
 from .env_params import EnvironmentParams
 from .meta_comp import MetaComponent
 
-__all__ = ["ComponentParser", "MetaComponent", "EnvironmentParams", "artifacts"]
+__all__ = [
+    "ComponentParser",
+    "MetaComponent",
+    "EnvironmentParams",
+]
