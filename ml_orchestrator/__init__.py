7b554ae296297eeebf220506ac6eca39f2f098e6
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-03-31 15:13:45 +0300

    Add version details to ml-orchestrator's init file
    The change has been made to include version information from the metadata in the ml-orchestrator module's __init__.py file. This will provide easy access to the module's version information, making it readily available for debugging, documentation, and other processes that need version tracking.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- ml_orchestrator/__init__.py
+++ ml_orchestrator/__init__.py
@@ -1,7 +1,10 @@
+from importlib.metadata import version
+
 from .comp_parser import ComponentParser
 from .env_params import EnvironmentParams
 from .meta_comp import MetaComponent
 
+__version__ = version("ml-orchestrator")
 __all__ = [
     "ComponentParser",
     "MetaComponent",
