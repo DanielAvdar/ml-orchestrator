a2135c6517cfb9ab89e6004ed43b8e73cdb3d523
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-11-17 04:34:09 +0200

    test imports.
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    from ml_orchestrator import (  # noqa
+        ComponentParser,
+        MetaComponent,
+        EnvironmentParams,
+        ProtocolCompSuite,
+        FunctionParser,
+    )
+
+    print("All tests passed!")
