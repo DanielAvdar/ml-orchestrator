308db478fd91c3203214d817d8825daf2448fec0
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-04-07 13:32:46 +0300

    Add Read the Docs config and update CI workflows/test script
    Introduced a Read the Docs configuration file for building documentation using Sphinx. Modified CI workflows to align with the new documentation process and replaced the test script entry point with `dummy_components.check_imports` for verifying imports. Added a new `check_imports.py` script to ensure core components are importable.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

---
+++ dummy_components/check_imports.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    from ml_orchestrator import (  # noqa
+        ComponentParser,
+        MetaComponent,
+        EnvironmentParams,
+        FunctionParser,
+    )
+
+    print("All tests passed!")
