b61de53b8001ebe56d90e5a5118a994da25d7ff5
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-04-08 15:51:11 +0300

    Add Sphinx references and restructure documentation
    Added internal references to all documentation sections for better navigation. Updated the index file to include separate ToC categories and switched the `master_doc` to "index" in the Sphinx configuration. This improves documentation structure and usability.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
-
+master_doc = "index"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
